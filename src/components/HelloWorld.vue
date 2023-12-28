bbd08c8ef8db6bd9f69735b8899f6bd0b72d4eca
nabilrt <nabilrt51@gmail.com>
2023-12-29 04:14:37 +0600

    counter and todo reactive app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+import WorkList from "./WorkList.vue";
+
+const count = ref(0);
+
+function increaseCount() {
+  count.value += 1;
+}
+function decreaseCount() {
+  count.value -= 1;
+}
+function resetCount() {
+  count.value = 0;
+}
+</script>
+
+<template>
+  <div
+    class="p-8 w-1/4 m-auto mt-72 border-2 text-center h-1/2 flex flex-col justify-center items-center space-y-5"
+  >
+    <p class="font-semibold text-black text-2xl">Count: {{ count }}</p>
+    <div class="flex space-x-2 mt-4">
+      <button
+        class="py-2 px-3 bg-green-400 hover:bg-green-700 text-white w-full rounded-md"
+        @click="increaseCount"
+      >
+        Increase
+      </button>
+      <button
+        class="py-2 px-3 bg-yellow-400 disabled:bg-yellow-200 hover:bg-yellow-600 text-white w-full rounded-md"
+        @click="decreaseCount"
+        :disabled="count === 0"
+      >
+        Decrease
+      </button>
+      <button
+        class="py-2 px-3 bg-red-400 disabled:bg-red-300 hover:bg-red-600 text-white w-full rounded-md"
+        @click="resetCount"
+        :disabled="count === 0"
+      >
+        Reset
+      </button>
+    </div>
+    <WorkList />
+  </div>
+</template>
---
+++ src/components/WorkList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+
+const allTodos = ref([
+  {
+    id: 1,
+    task: "Clean Home",
+    isCompleted: false,
+  },
+  {
+    id: 2,
+    task: "Do Homework",
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    task: "Learn Vue",
+    isCompleted: true,
+  },
+]);
+
+function changeStatus(index) {
+  const status = allTodos.value[index].isCompleted;
+  allTodos.value[index].isCompleted = !status;
+}
+</script>
+
+<template>
+  <div class="font-bold text-xl text-justify">My Work List</div>
+  <div v-for="(item, index) in allTodos">
+    <input
+      type="checkbox"
+      name=""
+      id=""
+      :checked="item.isCompleted"
+      @change="changeStatus(index)"
+    />
+    <span :class="item.isCompleted ? 'line-through' : ''"
+      >{{ item.id }} - {{ item.task }}</span
+    >
+  </div>
+</template>
